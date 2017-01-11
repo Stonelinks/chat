06a85b37e768f5df5adfd4ce78c89635792ab4b2
Lucas Doyle <ldoyle@airware.com>
2017-01-10 19:34:52 -0800

    brave new world

---
+++ src/TicTacToe.js
@@ -0,0 +1,318 @@
+/**
+ * The examples provided by Facebook are for non-commercial testing and
+ * evaluation purposes only.
+ *
+ * Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+'use strict'
+
+const React = require('react')
+const ReactNative = require('react-native')
+const {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} = ReactNative
+
+class Board {
+
+  constructor () {
+    const size = 3
+    const grid = Array(size)
+    for (let i = 0; i < size; i++) {
+      const row = Array(size)
+      for (let j = 0; j < size; j++) {
+        row[j] = 0
+      }
+      grid[i] = row
+    }
+    this.grid = grid
+
+    this.turn = 1
+  }
+
+  mark (row, col, player) {
+    this.grid[row][col] = player
+    return this
+  }
+
+  hasMark (row, col) {
+    return this.grid[row][col] !== 0
+  }
+
+  winner () {
+    for (let i = 0; i < 3; i++) {
+      if (this.grid[i][0] !== 0 && this.grid[i][0] === this.grid[i][1] &&
+          this.grid[i][0] === this.grid[i][2]) {
+        return this.grid[i][0]
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (this.grid[0][i] !== 0 && this.grid[0][i] === this.grid[1][i] &&
+          this.grid[0][i] === this.grid[2][i]) {
+        return this.grid[0][i]
+      }
+    }
+
+    if (this.grid[0][0] !== 0 && this.grid[0][0] === this.grid[1][1] &&
+        this.grid[0][0] === this.grid[2][2]) {
+      return this.grid[0][0]
+    }
+
+    if (this.grid[0][2] !== 0 && this.grid[0][2] === this.grid[1][1] &&
+        this.grid[0][2] === this.grid[2][0]) {
+      return this.grid[0][2]
+    }
+
+    return null
+  }
+
+  tie () {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.grid[i][j] === 0) {
+          return false
+        }
+      }
+    }
+    return this.winner() === null
+  }
+}
+
+const Cell = React.createClass({
+  cellStyle () {
+    switch (this.props.player) {
+      case 1:
+        return styles.cellX
+      case 2:
+        return styles.cellO
+      default:
+        return null
+    }
+  },
+
+  textStyle () {
+    switch (this.props.player) {
+      case 1:
+        return styles.cellTextX
+      case 2:
+        return styles.cellTextO
+      default:
+        return {}
+    }
+  },
+
+  textContents () {
+    switch (this.props.player) {
+      case 1:
+        return 'X'
+      case 2:
+        return 'O'
+      default:
+        return ''
+    }
+  },
+
+  render () {
+    return (
+      <TouchableHighlight
+        onPress={this.props.onPress}
+        underlayColor='transparent'
+        activeOpacity={0.5}>
+        <View style={[ styles.cell, this.cellStyle() ]}>
+          <Text style={[ styles.cellText, this.textStyle() ]}>
+            {this.textContents()}
+          </Text>
+        </View>
+      </TouchableHighlight>
+    )
+  }
+})
+
+const GameEndOverlay = React.createClass({
+  render () {
+    const board = this.props.board
+
+    const tie = board.tie()
+    const winner = board.winner()
+    if (!winner && !tie) {
+      return <View />
+    }
+
+    let message
+    if (tie) {
+      message = 'It\'s a tie!'
+    } else {
+      message = (winner === 1 ? 'X' : 'O') + ' wins!'
+    }
+
+    return (
+      <View style={styles.overlay}>
+        <Text style={styles.overlayMessage}>{message}</Text>
+        <TouchableHighlight
+          onPress={this.props.onRestart}
+          underlayColor='transparent'
+          activeOpacity={0.5}>
+          <View style={styles.newGame}>
+            <Text style={styles.newGameText}>New Game</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+})
+
+const TicTacToeApp = React.createClass({
+  getInitialState () {
+    return { board: new Board(), player: 1 }
+  },
+
+  restartGame () {
+    this.setState(this.getInitialState())
+  },
+
+  nextPlayer () {
+    return this.state.player === 1 ? 2 : 1
+  },
+
+  handleCellPress (row, col) {
+    if (this.state.board.hasMark(row, col)) {
+      return
+    }
+
+    this.setState({
+      board: this.state.board.mark(row, col, this.state.player),
+      player: this.nextPlayer()
+    })
+  },
+
+  render () {
+    const rows = this.state.board.grid.map((cells, row) =>
+      <View key={'row' + row} style={styles.row}>
+        {cells.map((player, col) =>
+          <Cell
+            key={'cell' + col}
+            player={player}
+            onPress={this.handleCellPress.bind(this, row, col)}
+          />
+        )}
+      </View>
+    )
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>EXTREME T3</Text>
+        <View style={styles.board}>
+          {rows}
+        </View>
+        <GameEndOverlay
+          board={this.state.board}
+          onRestart={this.restartGame}
+        />
+      </View>
+    )
+  }
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  title: {
+    fontFamily: 'Chalkduster',
+    fontSize: 39,
+    marginBottom: 20
+  },
+  board: {
+    padding: 5,
+    backgroundColor: '#47525d',
+    borderRadius: 10
+  },
+  row: {
+    flexDirection: 'row'
+  },
+
+  // CELL
+
+  cell: {
+    width: 80,
+    height: 80,
+    borderRadius: 5,
+    backgroundColor: '#7b8994',
+    margin: 5,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cellX: {
+    backgroundColor: '#72d0eb'
+  },
+  cellO: {
+    backgroundColor: '#7ebd26'
+  },
+
+  // CELL TEXT
+
+  cellText: {
+    borderRadius: 5,
+    fontSize: 50,
+    fontFamily: 'AvenirNext-Bold'
+  },
+  cellTextX: {
+    color: '#19a9e5'
+  },
+  cellTextO: {
+    color: '#b9dc2f'
+  },
+
+  // GAME OVER
+
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(221, 221, 221, 0.5)',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  overlayMessage: {
+    fontSize: 40,
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    fontFamily: 'AvenirNext-DemiBold',
+    textAlign: 'center'
+  },
+  newGame: {
+    backgroundColor: '#887765',
+    padding: 20,
+    borderRadius: 5
+  },
+  newGameText: {
+    color: 'white',
+    fontSize: 20,
+    fontFamily: 'AvenirNext-DemiBold'
+  }
+})
+
+AppRegistry.registerComponent('TicTacToeApp', () => TicTacToeApp)
+
+module.exports = TicTacToeApp
---
+++ webpack.prod.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { cacheDirectory: true }
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web'
+    }
+  }
+}
